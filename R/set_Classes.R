d237c2d32b359f7768305d7a07c1983941c0f347
Meera Patel <patelm9@mskcc.org>
2020-05-15 22:16:57 -0400

    set_Classes to give load order

---
+++ R/set_Classes.R
@@ -0,0 +1,36 @@
+#' Set TypewriteMessage Class
+#' @export TypewriteMessage
+#' @exportClass TypewriteMessage
+
+TypewriteMessage <- setClass("TypewriteMessage",
+                             representation(Plain = "character",
+                                            Formatted = "character"),
+                             prototype(Plain = NA_character_,
+                                       Formatted = NA_character_))
+
+
+#' Set the TypewriteLines Class
+#' @export TypewriteLines
+#' @exportClass TypewriteLines
+
+TypewriteLines <-
+    setClass("TypewriteLines",
+             representation(TypewriteMessage = "TypewriteMessage",
+                            "Blank Lines" = "numeric",
+                            "Indents" = "numeric"))
+
+
+#' Set TypewriteMessageLog Class
+#' @export TypewriteMessageLog
+#' @exportClass TypewriteMessageLog
+
+TypewriteMessageLog <-
+    setClass("TypewriteMessageLog",
+             representation(Timestamp = "character",
+                            "R Script" = "character",
+                            Message = "TypewriteMessage",
+                            Error = "character"
+             ),
+             prototype("R Script" = NA_character_,
+                       Error = NA_character_))
+
