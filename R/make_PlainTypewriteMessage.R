bf8bcf4b76fbb737053c9262c8691d92c35eb93b
Meera Patel <patelm9@mskcc.org>
2020-05-15 22:01:20 -0400

    add Typewrite S4 Class functions

--- R/make_PlainTypewriteMessage.R
+++ R/make_PlainTypewriteMessage.R
@@ -1,12 +1,10 @@
 #' Make a PlainTypewriteMessage
 #' @description This function fills the Plain slot of the TypewriteMessage object from the input
-#' @importFrom centipede rm_space_before_period
 #' @export
 
 make_PlainTypewriteMessage <-
     function(...) {
         TypewriteMessage_obj <- new("TypewriteMessage")
         TypewriteMessage_obj@Plain <- concat_words(...)
-        TypewriteMessage_obj@Plain <- centipede::rm_space_before_period(TypewriteMessage_obj@Plain)
         return(TypewriteMessage_obj)
     }
