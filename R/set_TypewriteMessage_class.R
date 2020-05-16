46fd219dbd53e82773ead20b5dbd8227e1fa3576
Meera Patel <patelm9@mskcc.org>
2020-05-15 22:17:31 -0400

    remove stale files

--- R/set_TypewriteMessage_class.R
+++
@@ -1,9 +0,0 @@
-#' Set TypewriteMessage Class
-#' @export TypewriteMessage
-#' @exportClass TypewriteMessage
-
-TypewriteMessage <- setClass("TypewriteMessage",
-                             representation(Plain = "character",
-                                            Formatted = "character"),
-                             prototype(Plain = NA_character_,
-                                       Formatted = NA_character_))
