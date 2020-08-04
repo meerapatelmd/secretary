43eafb3c55c6ade0f27899c7ef2361aaf11e6fa8
Meera Patel <patelm9@mskcc.org>
2020-08-04 10:22:58 -0400

    patch

---
+++ NEWS.md
@@ -0,0 +1,2 @@
+# Version 0.2.1  
+- Patch: add rlang import for list2 function call
--- R/make_PlainTypewriteMessage.R
+++ R/make_PlainTypewriteMessage.R
@@ -1,11 +1,12 @@
 #' Make a PlainTypewriteMessage
 #' @description This function fills the Plain slot of the TypewriteMessage object from the input
+#' @importFrom rlang list2
 #' @export
 
 make_PlainTypewriteMessage <-
     function(...) {
         concatenated_words <-
-                list2(...) %>%
+                rlang::list2(...) %>%
                 unlist() %>%
                 paste(collapse = " ")
         TypewriteMessage_obj <- new("TypewriteMessage")
