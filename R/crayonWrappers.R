b7567085258d13b3a2980fcc67afb5c201d6e929
Meera Patel <patelmeeray@gmail.com>
2020-08-10 23:43:15 -0400

    add crayon functions

---
+++ R/crayonWrappers.R
@@ -0,0 +1,131 @@
+#' Crayon Wrapper Functions
+#' @export
+#' @importFrom crayon italic
+
+italicize <-
+        function(...) {
+                crayon::italic(...)
+        }
+
+#' @export
+#' @importFrom crayon bold
+
+enbold <-
+        function(...) {
+                crayon::bold(...)
+        }
+
+
+#' @export
+#' @importFrom crayon underline
+
+uline <-
+        function(...) {
+                crayon::underline(...)
+        }
+
+
+#' @export
+#' @importFrom crayon inverse
+
+inside_out <-
+        function(...) {
+                crayon::inverse(...)
+        }
+
+#' @export
+#' @importFrom crayon strikethrough
+
+strikeout <-
+        function(...) {
+                crayon::strikethrough(...)
+        }
+
+#' @export
+#' @importFrom crayon black
+
+blackTxt <-
+        function(...) {
+                crayon::black(...)
+                }
+
+#' @export
+#' @importFrom crayon red
+
+redTxt <-
+        function(...) {
+
+                crayon::red(...)
+
+        }
+
+#' @export
+#' @importFrom crayon green
+
+greenTxt <-
+        function(...) {
+
+                crayon::green(...)
+
+        }
+
+#' @export
+#' @importFrom crayon yellow
+
+yellowTxt <-
+        function(...) {
+
+                crayon::yellow(...)
+
+        }
+
+#' @export
+#' @importFrom crayon blue
+
+blueTxt	 <-
+        function(...) {
+
+                crayon::blue(...)
+
+        }
+
+#' @export
+#' @importFrom crayon magenta
+
+magentaTxt <-
+        function(...) {
+
+                crayon::magenta(...)
+
+        }
+
+#' @export
+#' @importFrom crayon cyan
+
+cyanTxt	 <-
+        function(...) {
+
+                crayon::cyan(...)
+        }
+
+#' @export
+#' @importFrom crayon white
+
+whiteTxt <-
+        function(...) {
+
+                crayon::white(...)
+
+                }
+
+#' @export
+#' @importFrom crayon silver
+
+silverTxt <-
+        function(...) {
+
+                 crayon::silver(...)
+
+                }
+
+
